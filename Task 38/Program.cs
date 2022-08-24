f454c16134a9be2901f37a8f5a25b823fcaa0f0f
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-24 12:00:20 +0300

    Add task 38

---
+++ Task 38/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.*/
+double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
+
+double GetMaxMin(double[] mass)
+{
+    double max = mass[0];
+    double min = mass[0];
+    for (int i = 1; i < mass.Length; i++)
+    {
+        if (max < mass[i]) max = mass[i];
+        if (min > mass[i]) min = mass[i];
+    }
+    double result = max - min;
+    return result; 
+}
+
+double[] array = GetArray(10, 0, 20);
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine(String.Join(" ", GetMaxMin(array)));
