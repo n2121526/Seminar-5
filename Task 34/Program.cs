58b26e08710a1f2d48662c3e103ccbcdeecf67c6
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-24 11:54:46 +0300

    Add Task 34

---
+++ Task 34/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.*/
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        res [i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int EvenNum(int[] mas)
+{
+    int count = 0;
+    for (int i = 0; i < mas.Length; i++)
+    {
+      if (mas[i] % 2 == 0)
+          {
+               count++;
+          }
+    }
+    return count;
+}
+int[] array = GetArray(5, 100, 1000);
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine(String.Join(" ", EvenNum(array)));
+
